1ac13c6445ad423c6daa1a1fd4a5a67ad39119d6
lauluah <lauramarques181@gmail.com>
2025-12-28 11:15:20 -0300

    chore: estrutura inicial do projeto Spring Boot

---
+++ meusmapass/src/main/java/com/maps/meusmapass/MeusmapassApplication.java
@@ -0,0 +1,13 @@
+package com.maps.meusmapass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeusmapassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeusmapassApplication.class, args);
+	}
+
+}
