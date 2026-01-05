be2ec00387610865f96950b17e5dad6730783e86
lauluah <lauramarques181@gmail.com>
2026-01-05 17:50:40 -0300

    refactor(dto): cria PontoUpdateDTO para atualização de ponto

--- meusmapass/src/main/java/com/maps/meusmapass/dto/request/PontoUpdateDTO.java
+++ meusmapass/src/main/java/com/maps/meusmapass/dto/request/PontoUpdateDTO.java
@@ -1,4 +1,18 @@
 package com.maps.meusmapass.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class PontoUpdateDTO {
+
+    @NotBlank(message = "Nome é obrigatório")
+    @Size(min = 3, max = 20, message = "Nome deve ter entre 3 e 20 caracteres")
+    private String nome;
+
+    private String descricao;
+
 }
